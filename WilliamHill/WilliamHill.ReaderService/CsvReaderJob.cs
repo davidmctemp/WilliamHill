88bb8ab548e1ffad631c1ea6c80ec33cf685ab7e
David McEleney <davidmceleney@gmail.com>
2015-08-12 00:23:09 +1000

    Added importer skeleton and data project.

---
+++ WilliamHill/WilliamHill.ReaderService/CsvReaderJob.cs
@@ -0,0 +1,29 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WilliamHill.ReaderService
+{
+    public class CsvReaderJob : IJob
+    {
+        public CsvReaderJob()
+        {
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                // read csv values
+                Console.WriteLine("readong....");
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }       
+    }
+}
---
+++ WilliamHill/WilliamHill.ReaderService/Program.cs
@@ -0,0 +1,33 @@
+﻿using Quartz;
+using System.Configuration;
+using Topshelf;
+using Topshelf.Quartz;
+
+namespace WilliamHill.ReaderService
+{
+    class Program
+    {
+        private static int Main(string[] args)
+        {
+            return (int)HostFactory.Run(svc =>
+            {
+                svc.ScheduleQuartzJobAsService(job =>
+                {
+                    job
+                        .WithJob(() => JobBuilder.Create<CsvReaderJob>().Build())
+                        .AddTrigger(
+                            () =>
+                                TriggerBuilder.Create()
+                                    .WithCronSchedule(ConfigurationManager.AppSettings["CsvImporter"])
+                                    .Build());
+                });
+
+                svc.RunAsLocalSystem();
+
+                svc.SetDescription("WilliamHill.CsvImporter");
+                svc.SetDisplayName("WilliamHill.CsvImporter");
+                svc.SetServiceName("WilliamHill.CsvImporter");
+            });
+        }
+    }
+}
